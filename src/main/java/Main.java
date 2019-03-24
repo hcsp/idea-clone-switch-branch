ab13cae29ee9a7a78cb3fedf56ee648f65418968
Bo Zhang <bo@gradle.com>
2019-03-24 08:04:19 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import java.util.Base64;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Base64.getEncoder().encodeToString("Hello, world!".getBytes()));
+    }
+}
