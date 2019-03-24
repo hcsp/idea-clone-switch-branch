3dfa12390245132da62ded2107dc016c58d9b1ef
Bo Zhang <bo@gradle.com>
2019-03-24 08:05:25 +0800

    New branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import java.util.Base64;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Base64.getEncoder().encodeToString("Hello, world!".getBytes()));
+        System.out.println(Base64.getEncoder().encodeToString("Hello, hard core space!".getBytes()));
     }
 }
